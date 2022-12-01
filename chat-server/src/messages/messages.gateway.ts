40b5578dd4ba856723fab185175015b4ac126e6e
Rupesh Budhathoki <54392112+Roopaish@users.noreply.github.com>
2022-12-01 11:20:47 +0545

    removed unnecessary files

--- chat-server/src/messages/messages.gateway.ts
+++ chat-server/src/messages/messages.gateway.ts
@@ -12,7 +12,7 @@ import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
-    origin: '*',
+    origin: 'https://roopaish-chat.vercel.app/',
   },
 })
 export class MessagesGateway {
